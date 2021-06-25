a3b84a7dd0d992f12970f57216122e9ef4a36976
zawamlansarallah1 <66381319+zawamlansarallah1@users.noreply.github.com>
2021-06-25 06:34:26 +0200

    Update AutoDM.js

--- src/AutoDM.js
+++ src/AutoDM.js
@@ -41,7 +41,7 @@ const GenerateMessage = name => {
   ];
   const d = new Date();
   const dayName = days[d.getDay()];
-  return `Hi ${name} Thanks for .... \n Happy ${dayName} 😊😊 `; // your message
+  return `مرحبا  ${name} شكرا لمتابعة حساب زوامل أنصار الله  .... \n  نتمنى منك التفاعل مع الحساب  ودعمة عبراعادة التغريد  ومشاركتة لمتابعيك   😊😊\n تقبلوا خالص تحايانا  `; // your message
   // My message   return `Hi ${name} Thanks for being a part of my social media network. I'am the @PicsrushE founder,A new Online Image Editor completely with web technologies,I'm also a reactjs developer and medium blogger.\n Happy to discuss anytime 😊  \n Happy ${dayName} 😊😊 `;
 };
 
